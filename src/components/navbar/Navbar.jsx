21345ff7713044ad02bede2e598ecb8240f83a6e
j-hitesh <h26545080@gmail.com>
2025-04-19 23:27:05 +0530

    remove nav icon bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState} from 'react';
+import React from 'react';
 // import './Navbar.css';
 // import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
@@ -10,11 +11,11 @@ const Header = () => {
 const menushow = () => {
   if (!showMenuBar) {
     setShowMenuBar(true);
-    console.log(showMenuBar);
+    // console.log(showMenuBar);
   }
   else if(showMenuBar) {
     setShowMenuBar(false);
-    console.log(showMenuBar);
+    // console.log(showMenuBar);
   }
 };
 
@@ -25,10 +26,16 @@ const menushow = () => {
       {/* <img  src={assets.logo} alt="logo" className="w-12" /> */}
       <div>
        <button onClick={menushow} className='block sm:hidden'><i className="fa-solid fa-bars fixed top-5"></i></button>
-      <ul className={`top-8 shadow-xl shadow-gray-400 rounded-r-lg -left-5 pt-5 relative px-5 w-70 text-2xl ${showMenuBar ? 'h-screen bg-gray-700' : 'hidden'}`}>
-        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active" : "font-Tinos hover:text-red-500"}> <Link to='/'>Home</Link></li>
-        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>Reviews</Link></li>
-        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> Cart</Link></li>
+      <ul className={`top-8 shadow-xl shadow-gray-400 rounded-r-lg -left-5 pt-5 relative px-5 w-70 text-2xl  ${showMenuBar ? 'h-screen bg-gray-700 sm:hidden ' : 'hidden'}`}>
+        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active" : "font-Tinos hover:text-red-500"}> <Link to='/'>
+          <button onClick={menushow} >Home</button>
+         </Link></li>
+        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>
+        <button onClick={menushow} >Reviews</button>
+        </Link></li>
+        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> 
+        <button onClick={menushow} >Cart</button>
+        </Link></li>
       </ul>
       </div>
       <ul className="sm:flex gap-x-12  w-1/2 text-md lg:text-2xl sm:text-xl hidden pr-400 ">
