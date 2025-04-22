888a2ef35d9da527d263dcb517be6a580ca2d3ab
j-hitesh <h26545080@gmail.com>
2025-04-22 11:06:13 +0530

    give  active  color red

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -28,21 +28,21 @@ const menushow = () => {
       <div>
        <button onClick={menushow} className='block sm:hidden'><i className="fa-solid fa-bars fixed top-5"></i></button>
       <ul className={`top-8 shadow-xl shadow-gray-400 rounded-r-lg -left-5 pt-5 relative px-5 w-70 text-2xl  ${showMenuBar ? 'h-screen bg-gray-700 sm:hidden ' : 'hidden'}`}>
-        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active" : "font-Tinos hover:text-red-500"}> <Link to='/'>
+        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active , text-red-700" : "font-Tinos  hover:text-red-500"}> <Link to='/'>
           <button onClick={menushow} >Home</button>
          </Link></li>
-        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>
+        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active, text-red-700" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>
         <button onClick={menushow} >Reviews</button>
         </Link></li>
-        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> 
+        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active, text-red-700" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> 
         <button onClick={menushow} >Cart</button>
         </Link></li>
       </ul>
       </div>
       <ul className="sm:flex gap-x-12  w-1/2 text-md lg:text-2xl sm:text-xl hidden pr-400 ">
-        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active" : "font-Tinos hover:text-red-500"}> <Link to='/'>Home</Link></li>
-        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>Reviews</Link></li>
-        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> Cart</Link></li>
+        <li onClick={() => setMenu("Home")} className= {menu === "Home" ? "active, text-red-700" : "font-Tinos hover:text-red-500"}> <Link to='/'>Home</Link></li>
+        <li onClick={() => setMenu("Reviews")} className={menu === "Reviews" ? "active, text-red-700" : " font-Tinos hover:text-red-500"}><Link to='/Reviews'>Reviews</Link></li>
+        <li onClick={() => setMenu("Cart")} className={menu === "Cart" ? "active, text-red-700" : "font-Tinos hover:text-red-500"}><Link to='/Cart'> Cart</Link></li>
       </ul >
       
       
