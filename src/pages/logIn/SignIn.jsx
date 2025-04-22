dc486a5f28b79b2f131e0e15a706a144c5c95b51
j-hitesh <h26545080@gmail.com>
2025-04-22 11:38:57 +0530

    responsive site

--- src/pages/logIn/SignIn.jsx
+++ src/pages/logIn/SignIn.jsx
@@ -24,19 +24,19 @@ const SignIn = () => {
     }
   };
   return (
-    <div className='w-120 h-100 mx-auto mt-30 shadow-lg rounded-lg bg-orange-100'>
+    <div className='w-90 md:w-120 h-100 mx-auto mt-30 shadow-lg rounded-lg bg-orange-100'>
       <h1 className='text-5xl text-center text-red-600 text-lg pt-10 mt-20'>LOG IN</h1>
       <form className='flex flex-col items-center mt-10'>
-        <input type="text" placeholder='Gmail & Number' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white'
+        <input type="text" placeholder='Gmail & Number' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white'
          value={input}
          onChange={(e) => setInput(e.target.value.trim())}
         required min={9}  />
         {message && <p>{message}</p>}
-        <input type="password" placeholder='Password' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white' required />
+        <input type="password" placeholder='Password' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white' required />
         
-        <button  onClick={handleSubmit} className='bg-blue-500 text-white px-39 py-2 rounded-lg' id='login'>Log In</button>
+        <button  onClick={handleSubmit} className='bg-blue-500 text-white px-34 md:px-39 py-2 rounded-lg' id='login'>Log In</button>
           <p className='text-center pt-2 '>OR</p>
-        <button type="button" className='bg-red-700 text-white px-39 py-2 rounded-lg mt-4' onClick={() => setShowSign("sign") }>
+        <button type="button" className='bg-red-700 text-white px-34 md:px-39 py-2 rounded-lg mt-4' onClick={() => setShowSign("sign") }>
           <Link to='/signIn'>Sign In</Link>
         </button>
       </form>
--- src/pages/signIn/Sign.jsx
+++ src/pages/signIn/Sign.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const Sign = () => {
   return (
-    <div className='w-120 h-auto mx-auto mt-30 shadow-lg rounded-lg bg-orange-100'>
+    <div className=' w-90 md:w-120 h-auto mx-auto mt-30 shadow-lg rounded-lg bg-orange-100'>
       <h1 className='text-5xl text-center text-red-600 text-lg pt-10 mt-20'>SIGN IN</h1>
        <form className='flex flex-col items-center mt-10'>
-        <input type="text" placeholder='Name' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white' required />
-        <input type="number" placeholder='Number' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white no-spinner ' required Length={9} />
-        <input type="gmail" placeholder='Gmail' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white' />
-        <input type="password" placeholder='Password' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white' required />
-        <textarea placeholder='Address' className='border border-gray-300 rounded-lg p-2 mb-4 w-90 bg-white' required></textarea>
+        <input type="text" placeholder='Name' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white' required />
+        <input type="number" placeholder='Number' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white no-spinner ' required Length={9} />
+        <input type="gmail" placeholder='Gmail' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white' />
+        <input type="password" placeholder='Password' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white' required />
+        <textarea placeholder='Address' className='border border-gray-300 rounded-lg p-2 mb-4 w-80 md:w-90 bg-white' required></textarea>
         
-        <button type="submit" className='bg-blue-500 text-white px-39 py-2 rounded-lg mb-10'>Sign In</button>
+        <button type="submit" className='bg-blue-500 text-white px-34 md:px-39 py-2 rounded-lg mb-10'>Sign In</button>
       
         </form>
         
